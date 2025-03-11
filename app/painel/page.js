b43d0469f9bf4735b4b30242da82a74c68bc91d3
Murilo Santana Novais <95980479+MuriloSnovais@users.noreply.github.com>
2025-03-11 17:23:17 -0300

    Adicionando uma nova pagina de painel

---
+++ app/painel/page.js
@@ -0,0 +1,71 @@
+'use client'
+
+import { useState } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import "./painel.css"
+import { Trykker } from "next/font/google";
+
+export default function Painel(){
+
+    const [usuario, set_usuario] = useState("")
+    const [senha, set_senha] = useState("")
+    
+    const [mostra_erro, set_mostra_erro] = useState(false)
+    const [mostra_acerto, set_mostra_acerto] = useState(false)
+
+     
+    // const notify = () => toast("Acesso De adiministrador Liberado!");
+    // const notify2 = () => toast.error("Acesso De adiministrador Negado!");
+
+    function verifica_usuario() {
+        if (usuario == 'MulirinADM' && senha == "adm123"){
+            set_mostra_acerto(true)
+        }else{
+            set_mostra_erro(true)
+        }
+    }
+
+    return(
+        <div>
+            <h1>Painel de administrador</h1>
+
+            <label>
+                Digite o usuario:
+                <input type="email" onChange={(e)=> set_usuario(e.target.value)}/>
+            </label>
+            
+            <br/>
+
+            <label>
+                Digite a senha:
+                <input type="password" onChange={(e)=> set_senha(e.target.value)}/>
+            </label>
+
+            <br/>
+
+            <button onClick={()=> verifica_usuario()}>Entrar</button>
+            
+            {
+                mostra_erro == true ? 
+                <div className="erro">
+                    <p>Usuario ou senha incorreta</p>
+                </div>
+                :
+                    <div></div>  
+            }
+            {
+                mostra_acerto == true ?
+                <div className="acerto">
+                    <p>Usuario Logado com sucesso!</p>
+                </div>
+                :
+                    <div></div> 
+
+            }
+
+
+
+            <ToastContainer/>
+        </div>
+    )
+}
